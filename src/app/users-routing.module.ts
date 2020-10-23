8e425b46c9c251b46f4c2c74816ccd0e5ead085c
anton0380 <aa@unelma.ru>
2020-10-23 19:29:41 +0300

    correct routing and add service for users

--- src/app/users-routing.module.ts
+++ src/app/users-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const usersRoutes: Routes = [
+
+];
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(usersRoutes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class UsersRoutingModule { }
