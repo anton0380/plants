b084f21cb87a62fe26496225a83d55874500cad3
anton0380 <aa@unelma.ru>
2020-10-26 15:20:27 +0300

    users list component and correct users service

--- src/app/users.module.ts
+++ src/app/users.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { UsersRoutingModule } from './users-routing.module'
+import { UsersRoutingModule } from './users-routing.module';
+import { UserListComponent } from './user-list/user-list.component'
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserListComponent],
   imports: [
     CommonModule,
     FormsModule,
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+// import { Http, Response, URLSearchParams, RequestOptions } from '@angular/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { User } from './user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('php/users_get.php');
+  }
+
+  getUser(id:string): Observable<User> {
+
+    const params = new HttpParams().set('id', '3');
+    const options = {params: params};
+
+    return this.http.get<User>('php/users_get.php', options);
+  }
+
+  setUser(user: User): any {
+    const data = JSON.stringify(user);
+    return this.http.post('php/users_set.php', data);
+  }
+
+  deleteUser(id: number): any {
+    const data = JSON.stringify({id: id, mode: 'delete'});
+    return this.http.post('php/users_set.php', data);
+  }
 }
